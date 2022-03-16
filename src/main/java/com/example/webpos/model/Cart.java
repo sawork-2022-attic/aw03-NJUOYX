e23304a74fe10d5d3528e4e2e1a5b6ba3943e53c
NJUOYX <49577453+NJUOYX@users.noreply.github.com>
2022-03-16 19:26:03 +0800

    实现了基本功能

--- src/main/java/com/example/webpos/model/Cart.java
+++ src/main/java/com/example/webpos/model/Cart.java
@@ -9,9 +9,42 @@ import java.util.List;
 public class Cart {
 
     private List<Item> items = new ArrayList<>();
+    private double total = 0;
+
+    private void countTotal(){
+        total = 0;
+        for(Item item : items){
+            total += item.getQuantity()*item.getProduct().getPrice();
+        }
+    }
 
     public boolean addItem(Item item) {
-        return items.add(item);
+        items.add(item);
+        countTotal();
+        return true;
+    }
+
+    public void cancel(){
+        items.clear();
+        countTotal();
+    }
+
+    public void itemAmountAdd(int index, int amount){
+        items.get(index).setQuantity(items.get(index).getQuantity() + amount);
+        countTotal();
+    }
+
+    public void itemDelete(int index){
+        items.remove(index);
+        countTotal();
+    }
+
+    public void itemAmountDecline(int index, int amount){
+        items.get(index).setQuantity(items.get(index).getQuantity() - amount);
+        if(items.get(index).getQuantity()<=0){
+            itemDelete(index);
+        }
+        countTotal();
     }
 
     @Override
--- src/main/java/com/example/webpos/web/PosController.java
+++ src/main/java/com/example/webpos/web/PosController.java
@@ -6,11 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class PosController {
 
     private PosService posService;
+
+    private String refreshModel(Model model){
+        model.addAttribute("products", posService.products());
+        model.addAttribute("cart", posService.getCart());
+        return "index";
+    }
 
     @Autowired
     public void setPosService(PosService posService) {
@@ -19,9 +28,41 @@ public class PosController {
 
     @GetMapping("/")
     public String pos(Model model) {
-        posService.add("PD1",2);
-        model.addAttribute("products", posService.products());
-        model.addAttribute("cart", posService.getCart());
-        return "index";
+        return refreshModel(model);
+    }
+
+    @GetMapping("/add")
+    public String add(@RequestParam(name="pid")String pid, Model model){
+        posService.add(pid, 1);
+        return refreshModel(model);
+    }
+
+    @GetMapping("/item/remove")
+    public String itemRemove(@RequestParam(name="pid")String pid, Model model){
+        posService.getCart().itemAmountDecline(Integer.parseInt(pid), 1);
+        return refreshModel(model);
+    }
+
+    @GetMapping("/item/add")
+    public String itemAdd(@RequestParam(name="pid")String pid, Model model){
+        posService.getCart().itemAmountAdd(Integer.parseInt(pid), 1);
+        return refreshModel(model);
+    }
+
+    @GetMapping("/item/delete")
+    public String itemDelete(@RequestParam(name="pid")String pid, Model model){
+        posService.getCart().itemDelete(Integer.parseInt(pid));
+        return refreshModel(model);
+    }
+
+    @GetMapping("/cancel")
+    public String cancel(Model model){
+        posService.getCart().cancel();
+        return refreshModel(model);
+    }
+
+    @GetMapping("/charge")
+    public String charge(Model model){
+        return cancel(model);
     }
 }
